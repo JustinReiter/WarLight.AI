35b4f1423654206691a776016a560ff112ce6865
Justin Reiter <JDReiter@uwaterloo.ca>
2019-06-29 23:56:57 -0400

    Add in temporary logic for picking FTBs and Combos

--- JBot/Evaluation/PicksEvaluator.cs
+++ JBot/Evaluation/PicksEvaluator.cs
@@ -28,6 +28,7 @@ namespace WarLight.Shared.AI.JBot.Evaluation
 
             var pickableTerritories = BotState.DistributionStanding.Territories.Values.Where(o => o.OwnerPlayerID == TerritoryStanding.AvailableForDistribution).Select(o => o.ID).ToList();
             bool isAntWastelanded = false;
+            TerritoryIDType ausID = (TerritoryIDType) 0;
             var map = BotMap.FromStanding(BotState, BotState.DistributionStanding);
             var weights = pickableTerritories.ToDictionary(o => o, terrID =>
             {
@@ -36,6 +37,9 @@ namespace WarLight.Shared.AI.JBot.Evaluation
                 if (map.Territories[terrID].Bonuses[0].Details.Name.Equals("Antarctica") && BotState.BonusPickValueCalculator.IsWastelandedBonus(map.Territories[terrID].Bonuses[0]))
                 {
                     isAntWastelanded = true;
+                } else if (map.Territories[terrID].Bonuses[0].Details.Name.Equals("Australia"))
+                {
+                    ausID = terrID;
                 }
 
                 // Only find value if bonus has significiant amount
@@ -52,17 +56,16 @@ namespace WarLight.Shared.AI.JBot.Evaluation
                 }
             });
 
+            // Check for if pick is in Aus
+            if (!isAntWastelanded && weights.ContainsKey(ausID))
+            {
+                weights[ausID] -= 25;
+            }
 
             // Check for FTBs and assign value for Aus based on Ant Wasteland boolean
             List<ComboBonuses> firstTurnBonusList = new List<ComboBonuses>();
             foreach (KeyValuePair<TerritoryIDType, double> pick in weights)
             {
-                // Check for if pick is in Aus
-                if (map.Territories[pick.Key].Bonuses[0].Details.Name.Equals("Australia") && !isAntWastelanded)
-                {
-                    weights[pick.Key] -= 25;
-                }
-
                 // Checks for FTB
                 if (BotState.BonusPickValueCalculator.IsFirstTurnBonus(map.Territories[pick.Key].Bonuses[0]))
                 {
@@ -83,6 +86,30 @@ namespace WarLight.Shared.AI.JBot.Evaluation
             List<TerritoryIDType> picks = weights.OrderByDescending(o => o.Value).Take(maxPicks).Select(o => o.Key).Distinct().ToList();
             //StatefulFogRemover.PickedTerritories = picks;
 
+            AILog.Log("DEBUG", "BEFORE");
+            foreach (var terr in picks)
+            {
+                AILog.Log("DEBUG", map.Territories[terr].Details.Name);
+            }
+
+            for (int j = 0; j < firstTurnBonusList.Count; j++)
+            {
+                if (firstTurnBonusList[j].isFTB)
+                {
+                    for (int i = firstTurnBonusList[0].adjacentPickTerritories.Count - 1; i >= 0; i--)
+                    {
+                        BotTerritory temp = firstTurnBonusList[0].adjacentPickTerritories[i];
+                        picks.Remove(temp.ID);
+                        picks.Insert(0, temp.ID);
+                    }
+                }
+            }
+
+            AILog.Log("DEBUG", "BEFORE");
+            foreach (var terr in picks)
+            {
+                AILog.Log("DEBUG", map.Territories[terr].Details.Name);
+            }
             return picks;
         }
     }
