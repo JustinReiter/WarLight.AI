c7dd5fea34e43777855926752989e8981040e1c9
Justin Reiter <JDReiter@uwaterloo.ca>
2019-07-07 19:46:03 -0400

    Initial commit for Dijkstra's Algorithm

---
+++ JBot/GameObjects/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarLight.Shared.AI.JBot.Bot;
+
+namespace WarLight.Shared.AI.JBot.GameObjects
+{
+    class Node
+    {
+        private int shortestDistance;
+        private BotTerritory territory;
+
+        public Node(BotTerritory territory)
+        {
+            this.shortestDistance = Int32.MaxValue;
+            this.territory = territory;
+        }
+    }
+}
---
+++ JBot/GameObjects/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarLight.Shared.AI.JBot.Bot;
+
+namespace WarLight.Shared.AI.JBot.GameObjects
+{
+    class NaryTree
+    {
+        private Node root;
+
+        public NaryTree(BotTerritory root)
+        {
+            this.root = new Node(root);
+        }
+
+
+    }
+}
