b6f8630e25ae4dc9c7044bc14938a14b2af05c7b
Justin Reiter <JDReiter@uwaterloo.ca>
2019-08-08 00:04:56 -0400

    Create initial skeleton of deployment tracker

---
+++ JBot/Memory/DeploymentTracker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarLight.Shared.AI.JBot.Bot;
+
+namespace WarLight.Shared.AI.JBot.Memory
+{
+    static class DeploymentTracker
+    {
+        private static List<Dictionary<PlayerIDType, int>> deploysByTurn = new List<Dictionary<PlayerIDType, int>>();
+        
+
+
+
+        public static int GetDeploys(PlayerIDType playerId, BotMain bot)
+        {
+            if (GetTurns() <= bot.NumberOfTurns)
+            {
+                return deploysByTurn[bot.NumberOfTurns][playerId];
+            }
+            throw new IndexOutOfRangeException();
+        }
+
+        public static void SetDeploys(PlayerIDType playerId, int deploys, BotMain bot)
+        {
+            if (GetTurns() < bot.NumberOfTurns)
+            {
+                deploysByTurn.Add(new Dictionary<PlayerIDType, int>());
+            }
+            deploysByTurn[bot.NumberOfTurns][playerId] = deploys;
+        }
+
+
+        private static int GetTurns()
+        {
+            return deploysByTurn.Count;
+        }
+
+    }
+}
